b3c1b0d087af0872d3d5200e7f07cbd0aa3f5ecf
johanes.christian <johanes.christian@ui.ac.id>
2022-09-02 21:10:19 +0700

    Fix settings.py

--- django_rest_auth/settings.py
+++ django_rest_auth/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'corsheaders'
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
 ]
